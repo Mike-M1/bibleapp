f77038b5308c5bd4dc4e25e01a40605ab4bcfc76
Scooby doo <mikal.mckeever1@gmail.com>
2024-06-01 17:46:30 -0500

    best rn

--- python_server/myproject/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import home
-from .views import run_crew_view
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('run-crew/', run_crew_view, name="run_crew")
-]
